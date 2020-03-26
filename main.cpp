cab8f4d8e0dbaacb63f80328db57b897ac9b06cd
Vasi2105 <62296350+Vasi2105@users.noreply.github.com>
2020-03-26 14:12:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
 #include <string>
 using namespace std;
+char input[100] = "";
 
-int countSpaces(char input[])
+void read_message()
+{
+    cout << "Enter text: ";
+    cin.getline(input, 100);
+}
+
+int count_spaces(char input[])
 {
     int num = 0;
-    char space = ' ';
-    char letterRead;
-
-    for (int i = 0; input[i] > letterRead; i++)
+    char letterRead = input[0];
+    int i = 1;
+    while( letterRead != 0)
     {
-        if (letterRead = space);
-        num++;
+        if (letterRead == 32)
+            num += 1;
+        letterRead = input[i];
+        i++;
     }
-
+    cout<<i-1<<" caractere procesate."<<endl;
     return num;
+}
+
+void write_results()
+{
+
+    int spaces = count_spaces(input);
+    cout << "Number of spaces: " << spaces << endl;
 }
 
 int main()
 {
-    char input[100] = "\0";
-
-    cout << "Enter text: ";
-    cin.getline(input, 100);
-
-    int spaces = countSpaces(input);
-    cout << "Number of spaces: " << spaces << endl;
-
+    read_message();
+    write_results();
     return 0;
 }
